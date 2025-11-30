ee8b121fe37751dccd2b9d0c9bd85d2d1fe8a3ff
Maria Fernanda <119269054+fe-fe@users.noreply.github.com>
2025-11-30 19:12:42 -0300

    feat: get chunk byte frequency

--- main.py
+++ main.py
@@ -2,18 +2,26 @@ import os
 import time
 
 
-
-def get_file_stream(file_path: str, char_bytes: int = 1, char_qtd: int = 100):
+def get_binary_file_stream(file_path: str, chunk_bytes: int = 100):
     with open(file_path, "rb") as file:
         while True:
-            chunk = file.read(char_bytes*char_qtd)
+            chunk = file.read(chunk_bytes)
             if chunk:
                 yield chunk
-                
             else:
                 break
+
+        
+def get_chunk_byte_frequency(chunk: bytes, freq_dict: dict):
+    for byte in chunk:
+        freq_dict[byte] = freq_dict.get(byte, 0) + 1
+    return freq_dict        
             
                 
 if __name__ == "__main__":
-    for c in get_file_stream("test.txt", char_qtd=1):
-        print(f"{c}+")
+    freq_dict = dict()
+    for c in get_binary_file_stream("test.txt", 1):
+        get_chunk_byte_frequency(c, freq_dict)
+        
+    for item in freq_dict.items():
+        print(f"char: {bytes([item[0]]).decode()} freq: {item[1]}")
