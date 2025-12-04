fd13e120206e5ce4b72822dded8211a64ac454a8
Maria Fernanda <119269054+fe-fe@users.noreply.github.com>
2025-12-03 22:16:36 -0300

    feat: write compressed file
    todo:
    - function to build and write the header before writing the file content
    - function to read and interpret the header of a compressed file
    - function to decompress a file
    - refactor current compress method to make the code cleaner

--- huffman.py
+++ huffman.py
@@ -1,4 +1,5 @@
 from binary_tree import Node
+from file_utils import *
 
 
 def get_stream_byte_frequency(chunk: bytes, freq_dict: dict):
@@ -29,17 +30,61 @@ def build_huffman_frequency_tree(freq_dict: dict) -> Node:
     return node
 
 
-def get_codes(node: Node, code_list: list, current: str) -> list[tuple[bytes, str]]: # (data, code)
-    
+def get_codes(node: Node, code_dict: dict, current: str) -> dict[str, str]: # (data, code)
     if not node:
         return
     
     if node.is_leaf():
-        code_list.append(
-            (node.byte, current)
-        )
+        code_dict[node.byte] = current
     
-    get_codes(node.left, code_list, current + "0")
-    get_codes(node.right, code_list, current + "1")
+    get_codes(node.left, code_dict, "0" if not current else current + "0")
+    get_codes(node.right, code_dict, "1" if not current else current + "1")
     
-    return code_list
+    return code_dict
+
+
+def compress_chunk_bytes(chunk: bytes, code_dict: dict) -> str:
+    compressed_chunk = str()
+    for byte in chunk:
+        encoded_byte = code_dict.get(byte)
+        if not encoded_byte:
+            raise Exception(f"byte not found in code dictionary: [{byte}]")
+        compressed_chunk += encoded_byte
+    return compressed_chunk
+
+
+
+
+def compress_file(file_path: str) -> None:
+    compressed_file_path = build_compressed_file_name(file_path)
+    create_file(compressed_file_path)
+    for chunk in get_binary_file_stream(file_path, 1024):
+        freq_dict = dict()
+        get_stream_byte_frequency(chunk, freq_dict)
+    tree = build_huffman_frequency_tree(freq_dict)
+    code_dict = dict()
+    get_codes(tree, code_dict, str())
+    with open(compressed_file_path, "wb") as file_out:
+        
+        buffer_str = "" 
+        for chunk in get_binary_file_stream(file_path, 1024):
+            compressed_chunk = compress_chunk_bytes(chunk, code_dict)
+            buffer_str += compressed_chunk
+            bytes_to_write = bytearray()
+            
+            while len(buffer_str) >= 8:
+                byte_bits = buffer_str[:8]
+                buffer_str = buffer_str[8:]
+                byte_val = int(byte_bits, 2)
+                bytes_to_write.append(byte_val)
+        
+        
+            if len(bytes_to_write) > 0:
+                file_out.write(bytes_to_write)
+        
+        if len(buffer_str) > 0:
+            padding = 8 - len(buffer_str)
+            buffer_str += "0" * padding
+            final_byte = int(buffer_str, 2)
+            file_out.write(bytes([final_byte]))
+                
--- main.py
+++ main.py
@@ -1,13 +1,7 @@
 from file_utils import *
-from huffman import build_huffman_frequency_tree, get_stream_byte_frequency, get_codes
+import huffman
 import json
 
 
 if __name__ == "__main__":
-    freq_dict = dict()
-    for c in get_binary_file_stream("test.txt"):
-        get_stream_byte_frequency(c, freq_dict)
-    tree = build_huffman_frequency_tree(freq_dict)
-    code_list = list()
-    get_codes(tree, code_list, str())
-    print(code_list)
+    huffman.compress_file("test.txt")
